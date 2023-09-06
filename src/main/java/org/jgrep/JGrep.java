0a0caa1553b6d0bb679d8cb05c5b34a38e4432e5
Alex Kotovsky <alexktvsky@gmail.com>
2023-09-06 22:47:16 +0600

    Initial commit

---
+++ src/main/java/org/jgrep/JGrep.java
@@ -0,0 +1,13 @@
+package org.jgrep;
+
+public class JGrep {
+
+    void run(String[] args) {
+        System.out.println("Hello!");
+    }
+
+    public static void main(String[] args) {
+        JGrep main = new JGrep();
+        main.run(args);
+    }
+}
